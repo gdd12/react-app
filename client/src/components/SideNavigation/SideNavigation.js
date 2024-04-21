96f9e2f83dcb547d4d2762a351db407dc0224035
gdd12 <vytalpvp@gmail.com>
2024-04-20 20:54:21 -0700

    Added better styling to side navigation

--- client/src/components/SideNavigation/SideNavigation.js
+++ client/src/components/SideNavigation/SideNavigation.js
@@ -1,25 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import Logout from '../../helpers/Logout';
-
-// function SideNavigation() {
-//   return (
-//     <div className="sidebar">
-//       <ul>
-//         <li><Link to="/dashboard" className="nav-link">Dashboard</Link></li>
-//         <li><Link to="/profile" className="nav-link">Profile</Link></li>
-//         <li><Link to="/loans" className="nav-link">Loans</Link></li>
-//         <div className="logout">
-//           <li><Link to="/signin" className="nav-link logout" onClick={Logout}>Logout</Link></li>
-//         </div>
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default SideNavigation;
-
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logout from '../../helpers/Logout';
@@ -38,20 +16,21 @@ function SideNavigation() {
         <li><Link to="/profile" className="nav-link">Profile</Link></li>
         <li>
           <div className="nav-link" onClick={toggleDropdown}>
-            <Link>Loans ▼</Link>
-            {/* <span className={dropdownOpen ? "dropdown-icon open" : "dropdown-icon"}>▼</span> */}
+            <Link>Loans ↴</Link>
           </div>
           {dropdownOpen && (
             <ul className="dropdown-menu">
-              <li><Link to="/loans/add-loan" className="nav-link small">Add Loan</Link></li>
-              <li><Link to="/loans/add-payment" className="nav-link small">Add Payment</Link></li>
+              <li><Link to="/loans/add-loan" className="nav-link small"> - Add Loan</Link></li>
+              <li><Link to="/loans/add-payment" className="nav-link small">- Add Payment</Link></li>
             </ul>
           )}
         </li>
-        <div className="logout">
-          <li><Link to="/signin" className="nav-link logout" onClick={Logout}>Logout</Link></li>
-        </div>
       </ul>
+      <div className="logout">
+        <ul>
+          <li><Link to="/signin" className="nav-link logout" onClick={Logout}>Logout</Link></li>
+        </ul>
+      </div>
     </div>
   );
 }
