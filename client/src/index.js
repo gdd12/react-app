e8be0a030a5722c4a59f820e154f9be14694b9ba
gdd12 <vytalpvp@gmail.com>
2024-04-21 19:34:46 -0700

    Added font for all pages in the index file.

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import './components/SideNavigation/SideNavigation.css'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
